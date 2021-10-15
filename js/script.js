eac7c1b1d3f22c743fccf7ef50941b7103f349f9
LinaSeverus <severus@ukr.net>
2021-10-15 18:57:47 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+function calcExpon(num1, num2) {
+
+    if (isNaN(num1) || isNaN(num2)) {
+        return alert('Ошибка! Введено не число!');
+    }
+
+    if (typeof num2 === 'object' || num2 === '') {
+        calcExpon(num1, num2 = 1);
+    }
+    return num1 ** num2;
+
+}
+
+let userMessage = calcExpon(+prompt('Введите число'), prompt('В какую степень возвести первое число?'));
+alert(userMessage);
+
